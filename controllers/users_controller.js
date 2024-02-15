c84a24842a809fe42e5a20245d6bc637f849955e
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-02-15 18:47:21 +0530

    creating sign in and sign up page for the user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,14 @@
-//controllers/users_controller.js
-module.exports.profile =  function(req,res){
-    return res.end('<h1>usersController is working properly');
-}
+//users_controller.js
+const User = require("../models/users");
+//user the sign up page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "habit_tracker | Sign Up",
+  });
+};
+//setting up the sign in page
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "habit_tracker | Sign In",
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -2,5 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users_controller');
-router.get('/',usersController.profile);
+// router.get('/profile',usersController.Profile);
+router.get("/sign_up", usersController.signUp);
+router.get("/sign_in", usersController.signIn);
 module.exports = router;
